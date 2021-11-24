229c921ff736d9e4d288f5b6dcac397cea1ab091
ffforces <55713375+ffforces@users.noreply.github.com>
2021-11-24 19:14:44 +0200

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,384 @@
+let message1 = [
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi "
+];
+
+
+let message2 = [
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"float x;",
+"float d;",
+"float e;",
+"int index = 0;",
+"float b = 0;",
+"float r;",
+"float y;",
+"float u;",
+"float i;",
+"int p;",
+"float s;",
+"int z;",
+"int q;",
+"int h;",
+"float aika;",
+"void setup() {",
+"size(3840, 2160);",
+"frameRate(30);",
+"//fullScreen();",
+"//Helvetica = loadFont(Asul-Regular.ttf);",
+"//Helvetica=  createFont(Asul-Regular, 45);",
+"background(205,232,229);",
+"}",
+"void draw() {",
+"z++;",
+"aika=millis()*0.001;",
+"aika=aika%600;",
+"//teksti01",
+"if(aika<50){",
+"q++;",
+"r = int(random(100)+50);",
+"s=r+int(random(50));",
+"if(q>r){",
+"textFont(Helvetica, 45);",
+"fill(52, 106, 121);",
+"x = int(random(width)/3);",
+"text(message2[int(random(message2.length))],x+400,int(random(400)));",
+"//float w = textWidth(message[int(random(26))]);",
+"//index = (index + 1) % message.length;"
+];
+
+
+let message3 = [
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"en muista miltä näytän",
+"tulen koko ajan nuoremmaksi",
+"float x;",
+"float d;",
+"float e;",
+"int index = 0;",
+"float b = 0;",
+"float r;",
+"float y;",
+"float u;",
+"float i;",
+"int p;",
+"float s;",
+"int z;",
+"int q;",
+"int h;",
+"float aika;",
+"void setup() {",
+"size(3840, 2160);",
+"frameRate(30);",
+"//fullScreen();",
+"//Helvetica = loadFont(Asul-Regular.ttf);",
+"//Helvetica=  createFont(Asul-Regular, 45);",
+"background(205,232,229);",
+"}",
+"void draw() {",
+"z++;",
+"aika=millis()*0.001;",
+"aika=aika%600;",
+"//teksti01",
+"if(aika<50){",
+"q++;",
+"r = int(random(100)+50);",
+"s=r+int(random(50));",
+"if(q>r){",
+"textFont(Helvetica, 45);",
+"fill(52, 106, 121);",
+"x = int(random(width)/3);",
+"text(message2[int(random(message2.length))],x+400,int(random(400)));",
+"//float w = textWidth(message[int(random(26))]);",
+"//index = (index + 1) % message.length;"
+];
+
+
+let x;
+let d;
+let e;
+let index = 0;
+let b = 0;
+let r;
+let y;
+let u;
+let i;
+let p=1;
+let s=200;
+let z=1;
+let q=1;
+let h=1;
+let aika;
+
+
+function setup() {
+
+createCanvas(displayWidth, displayHeight);
+frameRate(30);
+
+//fullScreen();
+//Helvetica = loadFont("Asul-Regular.ttf");
+//Helvetica=  createFont("Asul-Regular", 45);
+//Helvetica = loadFont("CordiaUPC-48.vlw");
+background(205,232,229);
+}
+
+function draw() {
+
+
+z=z+1;
+aika=millis()*0.01;
+aika=aika%800;
+
+
+// //////////////////////////////////////////////////////////////////////////////
+
+//teksti01
+
+
+//fill(52, 106, 121);
+//x = int(random(width)/3);
+//let word = random(message1); // select random word
+//text(word, x+200, random(200)); // draw the word
+
+
+if(aika<50){
+
+q=q+1;
+
+r = random(100)+50;
+
+
+if(q>r){
+//textFont(Helvetica, 30);
+fill(52, 106, 121);
+x = int(random(width)/3);
+let word = random(message1); // select random word
+text(word, x+200, random(200)); // draw the word
+//text(message1[int(random(message1.length))],x+200,int(random(200)));
+//float w = textWidth(message[int(random(26))]);
+//index = (index + 1) % message.length;
+
+
+if(q>s){
+q=0;
+}
+}
+}
+
+
+
+//////////////////////////////////////////////////////
+
+
+//pyyhkimis funktio
+
+
+
+if(z>1800){
+
+fill(205,232,229);
+rect(0, 0, width, height);
+if(z>1802){
+z=0;
+
+}
+}
+
+////////////////////////////////////////////////////////////
+
+//keski sykkyrä
+if(aika<400){
+p++;
+h++;
+b = random(100)+1;
+u=b+random(300)+200;
+
+if(p>b){
+
+//textFont(Helvetica, 30);
+e = random(100)+450;
+
+let word2 = random(message2);
+fill(52, 106, 121);
+text(word2,e,random(100)+300);
+//float z = textWidth(message[int(random(26))]);
+//index = (index + 1) % message.length;
+}
+
+
+if(p>b){
+
+//textFont(Helvetica, 30);
+e = int(random(100))+450;
+let word3 = random(message3);
+fill(205,232,229);
+text(word3,e,random(100)+300);
+//float z = textWidth(message[int(random(26))]);
+//index = (index + 1) % message.length;
+}
+
+
+if(p>u){
+p=0;
+}
+}
+
+
+}
