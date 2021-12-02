03ef0b5475859d50cf9e289d78f1ceb6ec4b6292
ffforces <55713375+ffforces@users.noreply.github.com>
2021-12-02 11:40:37 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,21 +4,21 @@
 let message1 = ["päivä",
 "yö"];
 
-
-let message2 = ["kaikki tämä on yhä mutta mikään ei aina",
-"päivä juo sinua, silmistäsi paino putoaa sormiin",
+//päivä1
+let message2 = ["päivä juo sinua, silmistäsi paino putoaa sormiin",
 "joskus valo yllättää kulmasta jota ei nähty",
 "selkää pitkin nousee yhä ryppyisiä paitoja ",
-"eilinen kinostuu",
+"eilinen vastakinostunut",
 "on yhtä paljon hiljaisuutta kuin aikaa",
 "emme tunne värejämme",
 "vuodenaika rikkoo edeltäjänsä säännöt ",
 "katson hänen kasvojensa päiviä",
-"jäätyneen pinnan päälle sataa uutta lunta, minuutti tuntuu  "
+"jäätyneen pinnan päälle sataa uutta lunta ",
+"aukimurtunut, herkenevä makramee"
 ];
 
-let message3 =
-["kirjoittuneena hiuksiisi kuin rouhittu atlas ",
+//yö1
+let message3 = ["kirjoittuneena hiuksiisi kuin rouhittu atlas ",
 "aukeaa kuin kivuton lommo",
 "tulitikun pää on kuiva ja kysyy lisää pimeää",
 "myöhästymme toisin",
@@ -26,13 +26,43 @@ let message3 =
 "on yhtä paljon paikkoja, joista lähteä",
 "talvi, hidasta lähtöä ",
 "talvi kukkii, sitä vain ei kuule",
-"kaula-aukko, jonka läpi hänen pieni sähköä laulava päänsä",
+"kaula-aukko, hänen pieni sähköä laulava päänsä",
 "talvehdin, poskiluiden läpi alkavat alaäänet",
 "kuuntelemisen tilat aukeavat kuin kalkkiset onkalot",
-"auto sutinut lumeen urat jotka muistuttavat pimeässä reisiä"
+"auto sutinut lumeen urat kuin pimeän reidet"
 ];
 
-let a;
+let message4 = ["päivä",
+"yö"];
+
+//päiv2
+let message5 = ["painuu sinuun",
+"joskus valo ylittää kulmat jota ei nähty",
+"on enemmän hiljaisuutta kuin aikaa",
+"tunnen värejä",
+"vuodenaika rikkoo seuraajansa säännöt",
+"katselen hänen kasvojaan",
+"valoon kätkettyä pöytää",
+"jäätyneen pinnan päälle sataa minuutteja"
+];
+
+//yö2
+let message6 = ["vajonnut salonki",
+"vastakinostunut",
+"rouhittu atlas",
+"kivuton lommo",
+"herkenevä makramee",
+"myöhästymme enemmän kuin ennen",
+"kuuntelemisen tilat aukeavat ",
+"poskiluiden läpi alkavat alaäänet",
+"talvi kukkii, aukimurtunut",
+"pilaripuisto",
+"aukiluutunut, herkenevä makramee"
+]
+
+
+
+
 
 
 function setup() {
@@ -53,12 +83,13 @@ function draw() {
 
 let aika;
 aika=millis()*0.1;
-aika=aika%2;
+aika=aika%100;
 //a=random(2);
 fill(255,255,255);
 rect(0, 0, width, height);
 fill(0, 0, 0);
 
+if (aika <50){
 
 let word = random(message1); // select random word
 text(word, width*0.2, height*0.3); // draw the word
@@ -72,6 +103,23 @@ if (word == "yö"){
   let word2 = random(message3); // select random word
   text(word2, width*0.5, height*0.3); // draw the word
   }
+}
+
+if (aika >50){
+
+let word = random(message4); // select random word
+text(word, width*0.2, height*0.3); // draw the word
+
+if (word == "päivä"){
+let word2 = random(message5); // select random word
+text(word2, width*0.5, height*0.3); // draw the word
+}
+
+if (word == "yö"){
+  let word2 = random(message6); // select random word
+  text(word2, width*0.5, height*0.3); // draw the word
+  }
+}
 
 
 
