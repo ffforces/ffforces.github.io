7b92812153c382d033f49b4d6f33659301b1fa77
ffforces <55713375+ffforces@users.noreply.github.com>
2022-01-06 15:45:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-  import * as THREE from './three.js-master/build/three.module.js'
+  import * as THREE from 'three.module.js'
   import {GLTFLoader} from './three.js-master/examples/jsm/loaders/GLTFLoader.js'
     var audio = document.getElementById("audio");
     var started=false;
